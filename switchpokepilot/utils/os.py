f1cef50d2cd1c193b61ff9a0674de28a9872ab43
carimatics <github.carimatics@gmail.com>
2023-10-13 20:51:01 +0900

    refactoring utils/os.py

--- switchpokepilot/utils/os.py
+++ switchpokepilot/utils/os.py
@@ -1,5 +1,13 @@
-import os
+import platform
 
 
 def is_windows() -> bool:
-    return os.name == 'nt'
+    return platform.system() == 'Windows'
+
+
+def is_macos() -> bool:
+    return platform.system() == 'Darwin'
+
+
+def is_linux() -> bool:
+    return platform.system() == 'Linux'
