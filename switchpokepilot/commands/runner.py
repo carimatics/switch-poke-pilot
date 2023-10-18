4b9c5400607a868b0876573968a192f3bfca7828
carimatics <github.carimatics@gmail.com>
2023-10-18 09:13:58 +0900

    feat: to run command in background

--- switchpokepilot/commands/runner.py
+++ switchpokepilot/commands/runner.py
@@ -1,22 +1,29 @@
+import threading
+
 from switchpokepilot.commands.command import Command
 
 
 class CommandRunner:
     def __init__(self, command: Command | None = None):
         self.command: Command | None = command
+        self.__thread: threading.Thread | None = None
 
     @property
     def is_running(self):
-        if self.command is None:
+        if self.__thread is None:
             return False
-        return not self.command.should_running
+        return not self.__thread.is_alive()
 
     def start(self):
-        try:
-            self.command.preprocess()
-            self.command.process()
-        finally:
-            self.command.postprocess()
+        self.command.preprocess()
+
+        thread_name = f"{CommandRunner.__name__}:${self.command.process.__name__}:${self.command.name}"
+        self.__thread = threading.Thread(target=self.command.process,
+                                         name=thread_name,
+                                         daemon=True)
+        self.__thread.start()
 
     def stop(self):
         self.command.postprocess()
+        self.__thread.join()
+        self.__thread = None
