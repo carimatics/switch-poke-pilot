10777f833c6a2f66513a6c5ce2e111bce5eab699
carimatics <github.carimatics@gmail.com>
2023-10-21 17:08:39 +0900

    refactor: remove utils.logger

--- switchpokepilot/core/utils/logger.py
+++
@@ -1,9 +0,0 @@
-from logging import Logger, getLogger, NullHandler, DEBUG
-
-
-def get_app_logger(name: str) -> Logger:
-    logger = getLogger(name)
-    logger.addHandler(NullHandler())
-    logger.setLevel(DEBUG)
-    logger.propagate = True
-    return logger
