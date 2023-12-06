324420940f599f90af42ff37f6d8b6426970ec41
carimatics <github.carimatics@gmail.com>
2023-12-07 08:10:20 +0900

    refactor: example commands

--- examples/SwitchPokePilot/commands/hunt_ursaluna_bloodmoon/command.py
+++ examples/SwitchPokePilot/commands/hunt_ursaluna_bloodmoon/command.py
@@ -1,27 +1,14 @@
 # このコマンドは以下の実装を参考に作成しました。
 # https://github.com/Syumiru/Poke-Controller-Modified-PGM/blob/0dd26257521ebfe0f4fcc5fc2b8bc018e269beac/Commands/PythonCommands/ImageProcessingOnly/SV_A0_A0S0GACHIGUMA.py
 
-def check_should_keep_running(f):
-    def wrapper(*args):
-        self = args[0]
-        result = f(*args)
-        if not self.should_keep_running:
-            raise Exception("コマンドが中断されました")
-        return result
-
-    return wrapper
+from switch_controller_state import Button, StickDisplacementPreset
+from switch_pilot_core.command import BaseCommand, CommandAPI, check_should_keep_running
 
 
-class Command:
-    def __init__(self, api):
-        self.api = api
-        self.button = self.api.controller.button
-        self.stick = self.api.controller.stick
-
-        self.config = None
-
-        self.should_keep_running = False
-        self.is_alive = False
+class Command(BaseCommand):
+    def __init__(self, api: CommandAPI):
+        super().__init__(api)
+        self.cfg = None
 
         self.templates = {}
         self.capture_regions = {}
@@ -29,17 +16,14 @@ class Command:
 
     def process(self):
         try:
-            self.api.timer.start()
-            self.api.extensions.prepare(self)
-            self.config = self.api.config.read()
+            self.cfg = self.config.read()
 
             self.load_templates()
-
-            self.api.extensions.get_recognition(buttons=[self.button.ZL])
+            self.get_recognition()
 
             # main loop
             while self.should_keep_running:
-                self.api.extensions.attempt()
+                self.attempt()
                 self.log_info()
 
                 # Mash A
@@ -51,11 +35,11 @@ class Command:
 
                 # Check Speed
                 if self.detect_preemptive_attack():
-                    self.api.extensions.restart_sv()
+                    self.restart_sv()
                     continue
 
                 if not self.wait_for_battle_finish():
-                    self.api.extensions.restart_sv()
+                    self.restart_sv()
                     continue
 
                 # Catch
@@ -65,29 +49,29 @@ class Command:
                 # Check status
                 self.goto_status_screen()
                 if self.check_status():
-                    self.api.logger.info("目的の個体を捕獲しました。")
+                    self.logger.info("目的の個体を捕獲しました。")
                     self.log_info()
                     return
-                self.api.extensions.restart_sv()
+                self.restart_sv()
 
         except Exception as e:
-            self.api.logger.error(f"{e}")
+            self.logger.error(f"{e}")
 
         finally:
             self.log_info()
-            self.api.logger.info("終了します。")
-            self.api.timer.stop()
+            self.logger.info("終了します。")
+            self.timer.stop()
             self.postprocess()
 
     @check_should_keep_running
     def load_templates(self):
-        for name in self.config["templates"]:
-            config = self.config["templates"][name]
-            self.templates[name] = self.api.image.read_template(name=config["path"])
+        for name in self.cfg["templates"]:
+            config = self.cfg["templates"][name]
+            self.templates[name] = self.image.read_template(name=config["path"])
 
             x = config["captureRegion"]["x"]
             y = config["captureRegion"]["y"]
-            self.capture_regions[name] = self.api.image.create_region(x=(x[0], x[1]), y=(y[0], y[1]))
+            self.capture_regions[name] = self.image.create_region(x=(x[0], x[1]), y=(y[0], y[1]))
 
             self.thresholds[name] = config["threshold"]
 
@@ -97,7 +81,7 @@ class Command:
         template = self.templates[template_name]
         threshold = self.thresholds[template_name]
         while self.should_keep_running and not self.capture(template_name).contains(template, threshold):
-            self.api.controller.send_one_shot(buttons=[self.button.A], duration=0.05)
+            self.send_a(duration=0.05)
             self.wait(0.05)
 
     @check_should_keep_running
@@ -111,10 +95,10 @@ class Command:
 
     @check_should_keep_running
     def send_attack_command(self):
-        self.api.controller.send_repeat(buttons=[self.button.A],
-                                        times=2,
-                                        duration=0.05,
-                                        interval=1.0)
+        self.controller.send_repeat(buttons=[Button.A],
+                                    times=2,
+                                    duration=0.05,
+                                    interval=1.0)
         self.wait(1.7)
 
     @check_should_keep_running
@@ -141,41 +125,40 @@ class Command:
 
     @check_should_keep_running
     def catch(self):
-        self.api.controller.send_one_shot(buttons=[self.button.A],
-                                          duration=0.05)
+        self.send_a(duration=0.05)
         self.wait(0.6)
 
         # Select and throw ball
-        ball_index = self.config["catch"]["ballIndex"]
-        if self.config["catch"]["ballIndexSeekDirection"] == "right":
-            seek_direction = self.stick.RIGHT
+        ball_index = self.cfg["catch"]["ballIndex"]
+        if self.cfg["catch"]["ballIndexSeekDirection"] == "right":
+            seek_direction = StickDisplacementPreset.RIGHT
         else:
-            seek_direction = self.stick.LEFT
-        self.api.controller.send_repeat(l_stick=seek_direction,
-                                        times=ball_index,
-                                        duration=0.05,
-                                        interval=0.3,
-                                        skip_last_interval=False)
-        self.api.controller.send_one_shot(buttons=[self.button.A], duration=0.05)
+            seek_direction = StickDisplacementPreset.LEFT
+        self.controller.send_repeat(l_displacement=seek_direction,
+                                    times=ball_index,
+                                    duration=0.05,
+                                    interval=0.3,
+                                    skip_last_interval=False)
+        self.send_a(duration=0.05)
 
         # 演出待機
         self.wait(20)
 
     @check_should_keep_running
     def skip_pokedex(self):
-        if not self.config["catch"]["pokedexRegistered"]:
-            self.api.controller.send_one_shot(buttons=[self.button.A], duration=0.05)
+        if not self.cfg["catch"]["pokedexRegistered"]:
+            self.send_a(duration=0.05)
             self.wait(1.05)
 
     @check_should_keep_running
     def goto_status_screen(self):
-        self.api.controller.send_one_shot(l_stick=self.stick.BOTTOM)
-        self.api.controller.send_one_shot(buttons=[self.button.A])
+        self.send_down()
+        self.send_a()
         self.wait(1.0)
-        self.api.controller.send_one_shot(l_stick=self.stick.RIGHT)
+        self.send_right()
         self.wait(0.5)
-        if self.config["checkStatus"]["shouldSaveScreencapture"]:
-            self.api.video.capture()
+        if self.cfg["checkStatus"]["shouldSaveScreencapture"]:
+            self.screenshot()
 
     @check_should_keep_running
     def check_status(self):
@@ -187,12 +170,12 @@ class Command:
         template = self.templates[template_name]
         threshold = self.thresholds[template_name]
         contains_103 = self.capture(template_name).contains(template, threshold)
-        self.api.logger.info(f"攻撃(103): {contains_103}")
+        self.logger.info(f"攻撃(103): {contains_103}")
         return contains_103
 
     @check_should_keep_running
     def check_speed(self):
-        if not self.config["checkStatus"]["shouldCheckSpeed"]:
+        if not self.cfg["checkStatus"]["shouldCheckSpeed"]:
             return True
 
         template_name = "77"
@@ -200,41 +183,26 @@ class Command:
         template = self.templates[template_name]
         threshold = self.thresholds[template_name]
         contains_77 = capture.contains(template, threshold)
-        self.api.logger.info(f"素早さ(77): {contains_77}")
+        self.logger.info(f"素早さ(77): {contains_77}")
 
-        if self.config["checkStatus"]["speedIndividualValue"] == 0:
+        if self.cfg["checkStatus"]["speedIndividualValue"] == 0:
             return contains_77
 
         template_name = "78"
         template = self.templates[template_name]
         threshold = self.thresholds[template_name]
         contains_78 = capture.contains(template, threshold)
-        self.api.logger.info(f"素早さ(78): {contains_78}")
+        self.logger.info(f"素早さ(78): {contains_78}")
 
         return contains_77 or contains_78
 
     @check_should_keep_running
     def capture(self, name: str):
         capture_region = self.capture_regions[name]
-        return self.api.video.get_current_frame(region=capture_region).to_gray_scale()
+        return self.video.get_current_frame(region=capture_region).to_gray_scale()
 
     @check_should_keep_running
     def log_info(self):
-        elapsed_time = self.api.timer.elapsed_time
-        self.api.logger.info(f"経過時間: {elapsed_time.hours}時間{elapsed_time.minutes}分{elapsed_time.seconds}秒")
-        self.api.logger.info(f"実行回数: {self.api.extensions.attempt_count}")
-
-    @check_should_keep_running
-    def wait(self, duration: float):
-        self.api.extensions.wait(duration)
-
-    def preprocess(self):
-        self.should_keep_running = True
-        self.is_alive = True
-
-    def postprocess(self):
-        self.should_keep_running = False
-        self.is_alive = False
-
-    def stop(self):
-        self.should_keep_running = False
+        elapsed_time = self.elapsed_time
+        self.logger.info(f"経過時間: {elapsed_time.hours}時間{elapsed_time.minutes}分{elapsed_time.seconds}秒")
+        self.logger.info(f"実行回数: {self.attempt_count}")
--- examples/SwitchPokePilot/commands/mash_a/command.py
+++ examples/SwitchPokePilot/commands/mash_a/command.py
@@ -1,34 +1,16 @@
-class Command:
-    def __init__(self, api):
-        self.api = api
-        self.button = self.api.controller.button
+from switch_pilot_core.command import BaseCommand
 
-        self.should_keep_running = False
-        self.is_alive = False
 
+class Command(BaseCommand):
     def process(self):
         try:
-            self.api.timer.start()
-            self.api.extensions.prepare(self)
-
             # main loop
             while self.should_keep_running:
-                self.api.controller.send_one_shot(buttons=self.button.A)
-                self.api.extensions.wait(0.5)
+                self.send_a()
+                self.wait(0.1)
 
-            elapsed_time = self.api.timer.elapsed_time
+            elapsed_time = self.elapsed_time
             self.api.logger.info(f"経過時間 {elapsed_time.hours}時間{elapsed_time.minutes}分{elapsed_time.seconds}秒")
         finally:
-            self.api.timer.stop()
+            self.timer.stop()
             self.postprocess()
-
-    def preprocess(self):
-        self.should_keep_running = True
-        self.is_alive = True
-
-    def postprocess(self):
-        self.should_keep_running = False
-        self.is_alive = False
-
-    def stop(self):
-        self.should_keep_running = False
